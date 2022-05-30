9b2e3cc70013cbf63a38f7c6caafa5bd350818fc
aaron-llanos <aaron.soto@corebiz.ag>
2022-05-29 23:09:05 -0500

    prod front

---
+++ public/precache-manifest.087d4742c87aae09b63233ecdace574e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e580e81455abd788be294642a6898866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4867ed3569aebf24193",
+    "url": "/static/css/main.ec3e3507.chunk.css"
+  },
+  {
+    "revision": "fe2cf852acc170a5b02d",
+    "url": "/static/js/2.d1e2210a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1e2210a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4867ed3569aebf24193",
+    "url": "/static/js/main.e9149afd.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
